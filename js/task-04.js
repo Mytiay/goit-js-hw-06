f425b88fd557cbe65bccf398f1f0d37b497f90ab
Дмитрий <dimas.savin1990@gmail.com>
2022-10-30 12:57:21 -0400

    add

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+const counterValue = document.querySelector('span').textContent;
+console.log(counterValue);
+
+const addDecrement = document.querySelector('button[data-action="decrement"]');
+console.log(addDecrement);
+
+const addIncrement = document.querySelector('button[data-action="increment"]');
+console.log(addIncrement);
+
+addDecrement.addEventListener('click', () => {
+
+    
+});
+
+addIncrement.addEventListener("click", () => {
+
+});
