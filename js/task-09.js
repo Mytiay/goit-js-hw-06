c23bcd8f31084eb4bdfb1ad99bd7c6bee7a0fbe8
Дмитрий <dimas.savin1990@gmail.com>
2022-11-03 22:37:02 -0400

    task9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  span: document.querySelector(".color"),
+  button: document.querySelector(".change-color"),
+};
+console.log(refs.body);
+
+function onChangeBG  (event) {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = getRandomHexColor();
+};
+
+refs.button.addEventListener('click', onChangeBG);
