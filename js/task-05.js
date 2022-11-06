49f11ab2dd0754791d404273715af5a88d5bb932
Дмитрий <dimas.savin1990@gmail.com>
2022-11-05 20:54:23 -0400

    fix5

--- js/task-05.js
+++ js/task-05.js
@@ -6,7 +6,10 @@ const refs = {
 
 const onInputChange = (event) => {
     console.log(event.currentTarget.value);
-    refs.output.textContent = event.currentTarget.value;
+  refs.output.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    refs.output.textContent = "Anonymous";
+  }
 };
 
 refs.input.addEventListener("input", onInputChange);
